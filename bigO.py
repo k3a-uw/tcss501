eb0efe4a4c23ffcdf5b093040a03d4dc2c270f6c
k3a-uw <k3a@uw.edu>
2020-10-02 23:31:41 -0700

    Working Big O example code

--- bigO.py
+++ bigO.py
@@ -1,6 +1,7 @@
 import random
 import matplotlib.pyplot as plt
 import numpy as np
+import time
 
 
 
@@ -10,47 +11,40 @@ def array_reverse(input_list):
     Reverses the elements of the list to demonstrate a linear algorithm of reversing an array of things.
 
     :param input_list A list of objects to be reversed.
-    :return A tuple of the reversed list and a counter of the number of steps required to complete the action.
+    :return A returned version/copy of the list..
     """
 
-    inst_counter = 0
-
     array_len = len(input_list)
-    inst_counter += 1
 
     i = 0;
-    inst_counter += 1
 
     output_list = [None] * array_len
-    inst_counter += 1
 
     while i < array_len:
         output_list[i] = input_list[array_len-1 - i]
-        inst_counter += 1
         i += 1
 
-    return (output_list, inst_counter)
+    return output_list
 
 
 def bubble_sort(input_list):
-    inst_counter = 0
     for i in range(0, len(input_list)):
         for j in range(i+1, len(input_list)):
-            inst_counter += 1
             if input_list[i] > input_list[j]:
                 tmp = input_list[i]
                 input_list[i] = input_list[j]
                 input_list[j] = tmp
 
-    return (input_list, inst_counter)
+    return input_list
+
 
 def merge_sort(input_list):
     if len(input_list) > 1:
         split = len(input_list) // 2
         left = input_list[:split]
         right = input_list[split:]
-        left, inst_l = merge_sort(left)
-        right, inst_r = merge_sort(right)
+        left = merge_sort(left)
+        right = merge_sort(right)
 
         output = []
         left_index = 0
@@ -71,26 +65,24 @@ def merge_sort(input_list):
 
         while right_index < len(right):
             output.append(right[right_index])
-            right_index +=1
+            right_index += 1
 
-        return output, (inst_l + inst_r + inst_c)
-    return input_list, 0
-
-
+        return output
+    return input_list
 
 
 def draw_plot(x, y, title="My Cool Chart"):
     plt.ylabel("Steps to Complete")
     plt.xlabel("Input Size")
     plt.title(title)
-    plt.ylim(0, max(instructions)*1.05)
-    plt.plot(x , y, linestyle='--', marker='o')
+    plt.ylim(0, max(y)*1.05)
+    plt.plot(x, y, linestyle='--', marker='o')
     plt.show()
 
 
 # RUN DIFFERING SAMPLES
 MIN_SAMPLE_SIZE = 100
-MAX_SAMPLE_SIZE = 100000
+MAX_SAMPLE_SIZE = 10000
 NUM_SAMPLES = 100
 
 sample_sizes = np.linspace(MIN_SAMPLE_SIZE,
@@ -102,19 +94,25 @@ samples = []
 for sample_size in sample_sizes:
     samples.append([random.randint(1, 100) for i in range(0, sample_size)])
 
-instructions = []
+runtimes = []
 for sample in samples:
-    print(f"Reversing array of size: {len(sample)}")
-    r, i = array_reverse(sample)
-    instructions.append(i)
+    print(f"Reversing {len(sample)} element array...", end="")
+    start = time.perf_counter()
+    r = array_reverse(sample)
+    duration_seconds = time.perf_counter() - start
+    runtimes.append(duration_seconds)
+    print(f'{duration_seconds:0.00} seconds.', end="\r")
 
-draw_plot(sample_sizes, instructions, "Linear Algorithm: Reverse")
+print("\nDONE")
+
+draw_plot(sample_sizes, runtimes, "Linear Algorithm: Reverse")
 
 instructions = []
 for sample in samples:
     print(f"Bubble Sorting Array of len: {len(sample)}.")
     r, i = bubble_sort(sample)
     instructions.append(i)
+
 
 draw_plot(sample_sizes, instructions, "Quadratic Algorithm: Bubble Sort")
 
@@ -130,3 +128,7 @@ for sample in samples:
     instructions.append(i)
 
 draw_plot(sample_sizes, instructions, "n log n Algorithm: Merge Sort")
+
+for sample in samples[0:len(samples)//2]):
+
+
